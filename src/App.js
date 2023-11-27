927da825125af9fa6dd49c36042303864ecd894c
montyrajput <montysinghrahput590@gmail.com>
2023-11-27 16:52:43 +0000

    finalcommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import {Route, Switch, Redirect} from 'react-router-dom'
-import Login from './components/Login'
+import {
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom/cjs/react-router-dom.min'
+
 import Home from './components/Home'
+import Login from './components/Login'
+import Jobs from './components/Jobs'
 import NotFound from './components/NotFound'
+import JobItemDetails from './components/JobItemDetails'
 import ProtectedRoute from './components/ProtectRoute'
-import AboutJobItem from './components/AboutJobItem'
-import AllJobs from './components/AllJobs'
+
 import './App.css'
 
 // These are the lists used in the application. You can move them to any component needed.
+// const employmentTypesList = [
+//   {
+//     label: 'Full Time',
+//     employmentTypeId: 'FULLTIME',
+//   },
+//   {
+//     label: 'Part Time',
+//     employmentTypeId: 'PARTTIME',
+//   },
+//   {
+//     label: 'Freelance',
+//     employmentTypeId: 'FREELANCE',
+//   },
+//   {
+//     label: 'Internship',
+//     employmentTypeId: 'INTERNSHIP',
+//   },
+// ]
+
+// const salaryRangesList = [
+//   {
+//     salaryRangeId: '1000000',
+//     label: '10 LPA and above',
+//   },
+//   {
+//     salaryRangeId: '2000000',
+//     label: '20 LPA and above',
+//   },
+//   {
+//     salaryRangeId: '3000000',
+//     label: '30 LPA and above',
+//   },
+//   {
+//     salaryRangeId: '4000000',
+//     label: '40 LPA and above',
+//   },
+// ]
 
 // Replace your code here
 const App = () => (
-  <Switch>
-    <Route exact path="/login" component={Login} />
-    <ProtectedRoute exact path="/" component={Home} />
-    <ProtectedRoute exact path="/Jobs" component={AllJobs} />
-    <ProtectedRoute exact path="/Jobs/:id" component={AboutJobItem} />
-    <Route path="/not-found" component={NotFound} />
-    <Redirect to="not-found" />
-  </Switch>
+  <>
+    <Switch>
+      <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/jobs" component={Jobs} />
+      <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+      <Route exact path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
+    </Switch>
+  </>
 )
 
 export default App
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
-import {Link, withRouter} from 'react-router-dom'
-import {ImHome} from 'react-icons/im'
+import {AiFillHome} from 'react-icons/ai'
+import {BsBriefcaseFill} from 'react-icons/bs'
 import {FiLogOut} from 'react-icons/fi'
 import Cookies from 'js-cookie'
-import './index.css'
+// import { Link, withRouter } from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom/cjs/react-router-dom.min'
 
-const websiteLogo = 'https://assets.ccbp.in/frontend/react-js/logo-img.png'
+import './index.css'
 
 const Header = props => {
   const onClickLogout = () => {
@@ -12,34 +13,65 @@ const Header = props => {
     Cookies.remove('jwt_token')
     history.replace('/login')
   }
+
   return (
-    <nav className="nav-container">
-      <ul className="header-ul-container">
-        <li className="logo-container">
-          <Link className="link" to="/">
-            <img className="logo" src={websiteLogo} alt="website logo" />
-          </Link>
-        </li>
-        <li className="home-jobs-container">
-          <Link className="link" to="/">
-            <ImHome className="home-icon" />
-            <h1 className="nav-text">Home</h1>
-          </Link>
-          <Link className="link" to="/jobs">
-            <h1 className="nav-text">Jobs</h1>
-            <button className="home-jobs-btn" type="button">
-              Jobs
+    <header className="header-container">
+      <div className="mobile-navbar">
+        <Link to="/">
+          <img
+            className="header-logo"
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+          />
+        </Link>
+        <ul className="route-icons-container">
+          <li>
+            <Link to="/">
+              <button type="button">
+                <AiFillHome className="route-icon" />
+              </button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/jobs">
+              <button type="button">
+                <BsBriefcaseFill className="route-icon" />
+              </button>
+            </Link>
+          </li>
+          <li>
+            <button type="button" onClick={onClickLogout}>
+              <FiLogOut className="route-icon" />
             </button>
-          </Link>
-        </li>
-        <li>
-          <FiLogOut className="home-icon" onClick={onClickLogout} />
-          <button type="button" className="btn-logout" onClick={onClickLogout}>
-            Logout
-          </button>
-        </li>
-      </ul>
-    </nav>
+          </li>
+        </ul>
+      </div>
+      <div className="desktop-navbar">
+        <Link to="/">
+          <img
+            className="header-logo"
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+          />
+        </Link>
+        <ul className="route-items">
+          <li>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/jobs" className="nav-link">
+              Jobs
+            </Link>
+          </li>
+        </ul>
+
+        <button type="button" className="logout-button" onClick={onClickLogout}>
+          Logout
+        </button>
+      </div>
+    </header>
   )
 }
 
